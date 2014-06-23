3e4fab78ea388c31595ed33e05ef095fa9568d1b
lbrianza <lucabrianza@gmail.com>
2014-06-23 15:30:08 +0200

    update for 7_0_0

--- EcalCalibNtuple/python/simpleNtupleEoverP_cfi.py
+++ EcalCalibNtuple/python/simpleNtupleEoverP_cfi.py
@@ -1,5 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
+#from RecoJets.JetProducers.kt4PFJets_cfi import *
+#kt6PFJets = kt4PFJets.clone(rParam = 0.6, doAreaFastjet = cms.bool(True), src = cms.InputTag("particleFlow"), doPUOffsetCorr = cms.bool(False), doRhoFastjet = cms.bool(True))
+
+#from RecoJets.JetProducers.kt4PFJets_cfi import *
+#ak5PFJetsNew = kt4PFJets.clone( rParam = 0.6, doRhoFastjet = True, doAreaFastjet = cms.bool(True) )
+
 simpleNtupleEoverP = cms.EDAnalyzer(
     "SimpleNtupleEoverP",
     MCPileupTag         = cms.InputTag("addPileupInfo"),
@@ -11,7 +17,7 @@ simpleNtupleEoverP = cms.EDAnalyzer(
     digiCollection_EB   = cms.InputTag("ecalDigis","ebDigis"),
     digiCollection_EE   = cms.InputTag("ecalDigis","eeDigis"),
     theBeamSpotTag      = cms.InputTag("offlineBeamSpot"),
-    EleTag              = cms.InputTag("gsfElectrons"),
+    EleTag              = cms.InputTag("gedGsfElectrons"),
     PFMetTag            = cms.InputTag("pfMet"),
     MCtruthTag          = cms.InputTag("genParticles"),
     rhoTag              = cms.InputTag("kt6PFJets","rho"),
@@ -24,7 +30,7 @@ simpleNtupleEoverP = cms.EDAnalyzer(
     applyCorrections    = cms.untracked.bool(False),
     saveMCPU            = cms.untracked.bool(False),
     saveMCInfo          = cms.untracked.bool(False),
-    dataFlag            = cms.untracked.bool(True),
+    dataFlag            = cms.untracked.bool(False),
     saveRecHitMatrix    = cms.untracked.bool(False),
     saveFbrem           = cms.untracked.bool(False) # set False if running on AOD
     )
